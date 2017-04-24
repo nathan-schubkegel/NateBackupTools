c8296514e9e7d042ab9e564b16dbb4b093e88bbc
Nathan Schubkegel <nathan.schubkegel@gmail.com>
2017-04-24 00:30:11 -0700

    change output format of hashFolderContents.py to whitespace-separated CSV, and start new common code file

--- hashFolderContents.py
+++ hashFolderContents.py
@@ -1,49 +1,42 @@
 import sys
 import os
-import hashlib
 import argparse
+import nateBackupToolsCommon as common
+import csv
+import datetime
 
 # see https://docs.python.org/dev/library/argparse.html
-argParser = argparse.ArgumentParser(description='Reads all files in a directory and its subdirectories. It records file paths/sizes/MD5 hashes and writes them to an output file. Each line of the output file is: <hash> <sizeText> <sizeNumber> <path>')
+argParser = argparse.ArgumentParser(description='Reads all files in a directory and its subdirectories. It records file paths/sizes/MD5 hashes and writes them to an output file. The output file format is csv with space as the delimiter for the following fields: <hash> <sizeNumber> <sizeHumanReadableText> <path>')
 argParser.add_argument('dirToScan', help='The directory to scan')
 argParser.add_argument('outFilePath', help='The path to the output file to populate with MD5 hashes')
 args = argParser.parse_args()
 
-def getFileHash(filePath):
-  hasher = hashlib.md5()
-  with open(filePath, "rb") as f:
-    for chunk in iter(lambda: f.read(4096), b""):
-      hasher.update(chunk)
-    return hasher.hexdigest()
+# write binary because CSV writer requires that
+with open(args.outFilePath, "wb") as outFile:
+  csvOut = csv.writer(outFile, delimiter=' ', strict=True)
 
-def getHumanReadableSize(size):
-  for unit in ['-Bytes','-KB','-MB','-GB','-TB','-PB','-EB','-ZB']:
-    if abs(size) < 1024.0:
-      #return "%3.1f%s%s" % (size, unit, suffix)
-      return "%.0f%s" % (size, unit)
-    size /= 1024.0
-  return "%.0f%s" % (size, '-YB')
-
-with open(args.outFilePath, "w") as outFile:
   # inspect every file in the directory to scan
   for (dirPath, dirNames, fileNames) in os.walk(args.dirToScan):
     for fileName in fileNames:
       # get file size
       filePath = os.path.join(dirPath, fileName)
       fileSize = os.path.getsize(filePath)
-      fileSizeForHumes = getHumanReadableSize(fileSize)
+      fileSizeForHumes = common.getHumanReadableSize(fileSize)
 
       # show which file is being hashed
-      sys.stdout.write("reading " + filePath + " " + fileSizeForHumes + " ...");
+      sys.stdout.write("reading " + filePath + ", " + fileSizeForHumes);
       sys.stdout.flush();
 
       # get the file hash
-      filehash = getFileHash(filePath)
+      startTime = datetime.datetime.now()
+      fileHash = common.getFileHash(filePath)
+      endTime = datetime.datetime.now()
 
       # report the file hash
-      line = filehash + " " + fileSizeForHumes + " " + str(fileSize) + " " + filePath
-      sys.stdout.write("\r");
-      print(line)
-      outFile.write(line + "\n")
+      csvOut.writerow([fileHash, fileSize, fileSizeForHumes, filePath])
 
-#outFile.close()
+      # report how long it took
+      totalSeconds = int((endTime - startTime).total_seconds())
+      (hours, remainder) = divmod(totalSeconds, 3600)
+      (minutes, seconds) = divmod(remainder, 60)
+      sys.stdout.write(", %sh:%sm:%ss\n" % (hours, minutes, seconds))
---
+++ nateBackupToolsCommon.py
@@ -0,0 +1,104 @@
+
+import sys
+import os
+import ntpath
+import hashlib
+import re
+import datetime
+import argparse
+
+def getFileHash(filePath):
+  hasher = hashlib.md5()
+  with open(filePath, "rb") as f:
+    for chunk in iter(lambda: f.read(4096), b""):
+      hasher.update(chunk)
+    return hasher.hexdigest()
+
+def getHumanReadableSize(size):
+  for unit in ['bytes','KB','MB','GB','TB','PB','EB','ZB']:
+    if abs(size) < 1024.0:
+      return "%.0f %s" % (size, unit)
+    size /= 1024.0
+  return "%.0f %s" % (size, 'YB')
+
+def splitFilePath(filePath):
+    # use ntpath rather than os.path because it treats both kinds 
+    # of slashes as path separators
+
+    # drive indicators are annoying, so handle them separately
+    (drive, remainingPath) = ntpath.splitdrive(filePath)
+   
+    # os.path.split is weird about leading slashes, so capture them separately
+    leadingSlashes = ""
+    poo = re.search("^([\\/]+)(.*)$", remainingPath)
+    if poo:
+        leadingSlashes = poo.group(1)
+        remainingPath = poo.group(2)
+
+    # split dirs and filename
+    reversePathParts = []
+    while True:
+        (base, name) = ntpath.split(remainingPath)
+        if base == "":
+            if name == "":
+                raise ValueError("the file path was empty - not supported")
+            else:
+                reversePathParts.append(name)
+                break
+        else:
+            if name != "":
+                reversePathParts.append(name)
+                remainingPath = base
+            else:
+                raise ValueError("file path ends in a slash - not supported")
+
+    return (drive, leadingSlashes, reversePathParts)
+
+class MemFile:
+    def __init__(self, name, fileHash, fileSize, parentDir):
+        self.name = name
+        self.hash = fileHash
+        self.size = fileSize
+        self.dir = parentDir
+
+    def getPath(self):
+        return self.dir.getPath() + '/' + self.name
+
+class MemDirectory:
+    def __init__(self, name, parentDir):
+        self.files = {}
+        self.dirs = {}
+        self.name = name
+        self.dir = parentDir
+
+    def getPath(self):
+        if self.dir == None:
+            return self.name
+        return self.dir.getPath() + '/' + self.name
+
+    def add(self, fileHash, fileSize, reversePathParts):
+        name = reversePathParts[-1]
+        remainingParts = reversePathParts[:-1]
+        if len(reversePathParts) == 1:
+            newFile = MemFile(name, fileHash, fileSize, self)
+            self.files[name] = newFile
+            return newFile
+        else:
+            if name in self.dirs:
+                myDir = self.dirs[name]
+            else:
+                myDir = MemDirectory(name, self)
+                self.dirs[name] = myDir
+            return myDir.add(fileHash, fileSize, remainingParts)
+
+deduplicatedStrings = {}
+def deduplicate(parts):
+    i = 0
+    count = len(parts)
+    while i < count:
+        part = parts[i]
+        if part in deduplicatedStrings:
+            parts[i] = deduplicatedStrings[part]
+        else:
+            deduplicatedStrings[part] = part
+        i = i + 1
